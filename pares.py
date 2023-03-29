b1c30ff6f59576c6e74213811e2710c9535ba015
Isabela de Queiroz <117490695+isabela-code@users.noreply.github.com>
2023-03-29 08:59:59 -0300

    Add files via upload

---
+++ pares.py
@@ -0,0 +1,8 @@
+x = int(input('Digite um numero : '))
+
+while x != 0:
+    if x %2 != 0 :
+        x = x +1
+    soma = (5 * x) +20
+    print({soma})
+    x = int(input('Digite um numero : '))
---
+++ tabuada.py
@@ -0,0 +1,5 @@
+x = int(input('Deseja a tabuada de qual valor ? '))
+
+for i in range (1, 11):
+    tabuada = x * i 
+    print(f'{x} x {i} = {tabuada}')
